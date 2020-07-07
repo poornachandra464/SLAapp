5786f62a52de04bb9b96215de4c485057d4bad38
poornachandra464 <67284580+poornachandra464@users.noreply.github.com>
2020-07-06 23:06:16 -0700

    Add files via upload

---
+++ no-agents.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-agents',
+  templateUrl: './no-agents.component.html',
+  styleUrls: ['./no-agents.component.css']
+})
+export class NoAgentsComponent implements OnInit {
+  a:any;
+  b:any;
+  c:any;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  OnClickSave()
+  {
+   
+  }
+}
